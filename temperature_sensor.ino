ecc5d781cec1e94642d2b2e72667612922b6cc3d
Erik Cornelsen <eccornelsen@gmail.com>
2020-10-18 15:01:00 +0200

    analog input norm and scale

--- temperature_sensor.ino
+++ temperature_sensor.ino
@@ -1,36 +1,33 @@
 //constants
-const int AI_0 = A0; //TMP
-const int DI_2 = 2; //PB
-const int DO_3 = 3; //LED
-const float tmp_ref = 20.0;
+const unsigned int AI_0 = A0; //TMP
+const unsigned int DO_3 = 3; //LED
+const unsigned int tmp_min = 0;
+const unsigned int tmp_max = 1023;
 
 //variables
-int tmp_value = 0;
-int tmp_min = 1023;
-int tmp_max = 0;
+unsigned int tmp_value = 0;
 float tmp_volts = 0.0;
 float tmp_deg = 0.0;
 
 //void setup
 void setup() {
   //IO mapping
-  pinMode(DI_2, INPUT);
   pinMode(DO_3, OUTPUT);
   pinMode(AI_0, INPUT);
   analogReference(INTERNAL);
   //Enabling Serial port
   Serial.begin(9600);  
   
-  
   //Calibration
   digitalWrite(DO_3, HIGH);
-  while(millis() < 20000){
+  while(millis() < 5000){
     tmp_value = analogRead(AI_0);
+    Serial.println(tmp_value);
     if(tmp_value > tmp_max) {
-      tmp_max = tmp_value;
+      tmp_value = tmp_max;
     }
     if(tmp_value < tmp_min) {
-      tmp_min = tmp_value;
+      tmp_value = tmp_min;
     }
   }
   digitalWrite(DO_3, LOW);
@@ -38,24 +35,22 @@ void setup() {
 //void loop
 void loop() {
   tmp_value = analogRead(AI_0);
-  tmp_value = map(tmp_value, tmp_min, tmp_max, 0, 255);
-  tmp_value = constrain(tmp_value, 0, 255);  
-  tmp_volts = (tmp_value/1024.0)*5.0;
+  Serial.println(tmp_value);
+  //Analog signal Normalization and Scaling
+  tmp_value = map(tmp_value, tmp_min, tmp_max, tmp_min, tmp_max);
+  tmp_value = constrain(tmp_value, tmp_min, tmp_max);  
+  Serial.println(tmp_value);
+  tmp_volts = (tmp_value/(float)(tmp_max+1-tmp_min))*1.1;
+  Serial.println(tmp_volts);
   tmp_deg = (tmp_volts - 0.5)*100;
-
-  unsigned int tot;
-  tot = 0;
-  for (int i = 0; i < 64; i++){
-    tot += analogRead(AI_0);
-    }
-  
-  //Serial.print(tmp_value);
-  //Serial.print(tmp_volts);
-  Serial.print(tmp_deg);
-  Serial.print('-');
-  Serial.print(tot*0.001632 - 50.0);
-  Serial.print('\n');
-  
+  Serial.println(tmp_deg);
   delay(1000);
 
+  unsigned int avg=0;
+  for (int i = 0; i < 64; i++){
+    avg += analogRead(AI_0);
+    }
+  Serial.print("Average analog input ");
+  Serial.println(avg/64.0);
+  delay(500);
 }
