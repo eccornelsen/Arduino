0ac4890d4a9338c173995a676aed832eb4a287bd
Erik Cornelsen <eccornelsen@gmail.com>
2020-10-19 21:51:14 +0200

    internal 1.1V reference voltage

--- temperature_sensor.ino
+++ temperature_sensor.ino
@@ -1,56 +1,57 @@
 //constants
 const unsigned int AI_0 = A0; //TMP
 const unsigned int DO_3 = 3; //LED
-const unsigned int tmp_min = 0;
-const unsigned int tmp_max = 1023;
+const unsigned int max_dig = 1023; //10bit resolution
+const String mode = "INTERNAL";
 
 //variables
-unsigned int tmp_value = 0;
-float tmp_volts = 0.0;
-float tmp_deg = 0.0;
+float dig_value = 0.0;
+float temp_volts = 0.0;
+float temp_deg = 0.0;
+float Vref = 0.0;
 
 //void setup
 void setup() {
   //IO mapping
   pinMode(DO_3, OUTPUT);
   pinMode(AI_0, INPUT);
-  analogReference(INTERNAL);
+
+  //setting INTERNAL reference voltage
+  if(mode == "INTERNAL") {
+    analogReference(INTERNAL);
+    Vref = 1.1;
+    }
+
   //Enabling Serial port
   Serial.begin(9600);  
   
   //Calibration
   digitalWrite(DO_3, HIGH);
   while(millis() < 5000){
-    tmp_value = analogRead(AI_0);
-    Serial.println(tmp_value);
-    if(tmp_value > tmp_max) {
-      tmp_value = tmp_max;
-    }
-    if(tmp_value < tmp_min) {
-      tmp_value = tmp_min;
-    }
+    dig_value = analogRead(AI_0);
+    Serial.println(dig_value);
+    dig_value = constrain(dig_value, 0, max_dig);
   }
   digitalWrite(DO_3, LOW);
 }
 //void loop
 void loop() {
-  tmp_value = analogRead(AI_0);
-  Serial.println(tmp_value);
-  //Analog signal Normalization and Scaling
-  tmp_value = map(tmp_value, tmp_min, tmp_max, tmp_min, tmp_max);
-  tmp_value = constrain(tmp_value, tmp_min, tmp_max);  
-  Serial.println(tmp_value);
-  tmp_volts = (tmp_value/(float)(tmp_max+1-tmp_min))*1.1;
-  Serial.println(tmp_volts);
-  tmp_deg = (tmp_volts - 0.5)*100;
-  Serial.println(tmp_deg);
-  delay(1000);
-
+  //Analog value average read and limiting
   unsigned int avg=0;
   for (int i = 0; i < 64; i++){
-    avg += analogRead(AI_0);
+    dig_value = analogRead(AI_0);
+    dig_value = constrain(dig_value, 0, max_dig);
+    avg += dig_value;
     }
-  Serial.print("Average analog input ");
-  Serial.println(avg/64.0);
-  delay(500);
+  dig_value = avg/64.0;  
+  Serial.println(dig_value);
+  
+  //ADC conversion
+  temp_volts = (dig_value/(float)(max_dig+1))*Vref;
+  Serial.println(temp_volts);
+
+  //Temperature calculation
+  temp_deg = (temp_volts - 0.5)*100; //750mV @ 25degC 
+  Serial.println(temp_deg);
+  delay(1000);
 }
